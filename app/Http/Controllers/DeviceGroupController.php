be7bbcc9e27663139229f9d8c09dc3f9c71d1147
MustaphaYacine <mustaph.w012@gmail.com>
2018-07-02 15:44:51 +0100

    adding the deviceController add

--- app/Http/Controllers/DeviceGroupController.php
+++ app/Http/Controllers/DeviceGroupController.php
@@ -9,7 +9,8 @@ class DeviceGroupController extends Controller
 {
     //
     public function add(Request $request){
-
+        $this->validate($request,["group_name"=>"String|Required"],[$project_id]="Integer");
+        return Device_group::create(["group_name"=>$request->group_name,'project_id'=>$project_id]);
 
     }
 }
