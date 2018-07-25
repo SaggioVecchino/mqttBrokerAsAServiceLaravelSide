78eb4bb0c5ff3cc7673909b8197ac53e3d8c16d3
SaggioVecchino <rhliyy@gmail.com>
2018-07-25 15:51:59 +0100

    added a space to resolve a syntax error

--- resources/assets/js/components/editprojectpassword.vue
+++ resources/assets/js/components/editprojectpassword.vue
@@ -11,7 +11,7 @@
 
                             <label :for="passInputId(1)">Old password</label>
 
-                            <input :id="passInputId(1)" v-model="form.old_password"type="password" placeholder="Old password" class="form-control" name="old_password"  >
+                            <input :id="passInputId(1)" v-model="form.old_password" type="password" placeholder="Old password" class="form-control" name="old_password"  >
 
                             <span class="help is-danger invalid-feedback" style="display: inline" v-if="form.errors.has('old_password')" v-text="form.errors.get('old_password')"></span>
 
@@ -41,53 +41,53 @@
 </template>
 
 <script>
-    import modal from './modal.vue';
-    import Form from "../forms/form";
-    export default {
-
-        name: "editprojectpassword"
-        ,
-        components:{'modal':modal},
-        props:['project_id','project_name'],
-        data(){
-            return {
-                form: new Form({
-                    old_password:'',
-                    password:''
-                })
-            }},
-        methods: {
-            onSubmit() {
-                this.form.patch(`http://localhost:8000/projects/${this.project_id}/change_password`)
-                    .then(function (response ){
-                            window.location.href = 'http://localhost:8000/projects'
-                    })
-            },
-            addError(e){
-                this.form.onFail(e);
-            },
-            passInputId(i){
-                return `edit_project_password_input${i}`.concat(this.project_id)
-            }
-        },
-        mounted(){
-            var form=this.form
-            $("#".concat(`${this.modalId}`)).on('hidden.bs.modal', function (e) {
-                form.reset();
-            })
-
-        },
-        computed:{
-            modalId(){
-                return this.project_name.concat(this.project_id,'edit_password')
-            },
-            idInput(){
-                return `edit_project_password_input`.concat(this.project_id)
-            }
-        }
+import modal from "./modal.vue";
+import Form from "../forms/form";
+export default {
+  name: "editprojectpassword",
+  components: { modal: modal },
+  props: ["project_id", "project_name"],
+  data() {
+    return {
+      form: new Form({
+        old_password: "",
+        password: ""
+      })
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.form
+        .patch(
+          `http://localhost:8000/projects/${this.project_id}/change_password`
+        )
+        .then(function(response) {
+          window.location.href = "http://localhost:8000/projects";
+        });
+    },
+    addError(e) {
+      this.form.onFail(e);
+    },
+    passInputId(i) {
+      return `edit_project_password_input${i}`.concat(this.project_id);
     }
+  },
+  mounted() {
+    var form = this.form;
+    $("#".concat(`${this.modalId}`)).on("hidden.bs.modal", function(e) {
+      form.reset();
+    });
+  },
+  computed: {
+    modalId() {
+      return this.project_name.concat(this.project_id, "edit_password");
+    },
+    idInput() {
+      return `edit_project_password_input`.concat(this.project_id);
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
